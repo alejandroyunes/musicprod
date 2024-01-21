cf5f24a2ac6464114aa7c0704ffa82df4ee599ea
Alejandro Yunes <alejandroyunes@outlook.com>
2024-01-21 11:27:38 -0500

    chaging meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css"
 import { ThemeProvider } from "./providers"
 
 export const metadata = {
-  title: "Capital Invest",
-  description: "Capital invest company",
+  title: "Music Production",
+  description: "Music",
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
